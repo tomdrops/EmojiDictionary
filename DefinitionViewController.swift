4325f7ba30b88197f52a8a0d589b4b99edeac678
Thomas Gumpinger <tomdrops@mac.com>
2018-09-03 23:37:47 +0200

    Done

--- DefinitionViewController.swift
+++ DefinitionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DefinitionViewController: UIViewController {
     
+    @IBOutlet weak var definitionLabel: UILabel!
     
 
     @IBOutlet weak var emojiLabel: UILabel!
@@ -20,6 +21,25 @@ class DefinitionViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         emojiLabel.text = emoji
+        
+        if emoji == "🐻" {
+            definitionLabel.text = "A f...g Hamster!"
+        }
+        if emoji == "😁" {
+            definitionLabel.text = "A boring Smile!"
+        }
+        if emoji == "😍" {
+            definitionLabel.text = "Fool in Love!"
+        }
+        if emoji == "🤬" {
+            definitionLabel.text = "A lovely Complaint!!"
+        }
+        if emoji == "💩" {
+            definitionLabel.text = "The stinking Truth!"
+        }
+        if emoji == "👠" {
+        definitionLabel.text = "Red Shoes Pop!"
+        }
 
     }
     
