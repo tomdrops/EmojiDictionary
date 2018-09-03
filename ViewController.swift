ab97bbd5317b88a58189851fa475912f4a6ca1f8
Thomas Gumpinger <tomdrops@mac.com>
2018-09-03 22:49:57 +0200

    Added Navigation

--- ViewController.swift
+++ ViewController.swift
@@ -32,6 +32,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "movesegue", sender: "🎮")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
